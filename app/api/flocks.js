42cbad86dd1be82ad054a19cc360cdf1dea0aae5
Alexander Honer <djfunkymuffins@gmail.com>
2017-07-07 16:13:14 -0400

    better?

--- app/api/flocks.js
+++ app/api/flocks.js
@@ -2,6 +2,6 @@ const router = require('koa-router')();
 
 const flocks = require('../controllers/flocks.js')
 
-router.get('/', flocks.getFlocks);
+router.get('/flocks', flocks.getFlocks);
 
 module.exports = router.middleware();
